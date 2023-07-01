75a26a191cb976f02138f96153b6483ded991b96
avexKD <kopihideung.energy@hotmail.com>
2023-07-01 13:13:31 +0700

    Initial commit

---
+++ install/composer-setup.bat
@@ -0,0 +1,7 @@
+@echo off
+echo Menjalankan Composer Installer...
+cd /d "C:\DapurOperator\install"
+Composer-Setup.exe
+
+echo Install berhasil, lanjutkan
+
---
+++ install/pathpgsql.bat
@@ -0,0 +1,5 @@
+@echo off
+
+setx PATH "%PATH%;C:\DapurOperator\pgsql\bin" /M
+
+echo Path environment variable for pg_dump has been set.
